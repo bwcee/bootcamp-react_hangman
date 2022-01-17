1a074b9debd6b0668ca34a4b120e32c3888c5c57
bwcee <bwceemail@gmail.com>
2022-01-17 12:27:39 +0800

    use mix of both useState and useRef to store values

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 /* 
 1. all the different states, set state functions and constants passed here
@@ -6,31 +6,11 @@ import React, { useState } from "react";
 - set state -> re-render -> set state -> re-render ...
 - when component re-rendered, set state functions not called immediately cos set state functions are inside letterInputChange(). so set state functions only triggered when there is change in <input>
 */
-const Form = ({
-  secretWord,
-  guess,
-  setGuess,
-  guessedLetters,
-  setGuessedLetters,
-  hangMan,
-  setHangMan,
-}) => {
+const Form = ({ guess, setGuess }) => {
   const letterInputChange = (ev) => {
     ev.target.value.length > 1
       ? alert("only key in one letter!")
       : setGuess(ev.target.value);
-
-    if (guess != "" && !secretWord.includes(guess)) {
-      guessedLetters.push(hangMan.shift());
-    } else {
-      secretWord.forEach((letter, index) => {
-        if (guess == letter) {
-          guessedLetters[index] = guess;
-        }
-      });
-    }
-    setGuessedLetters([...guessedLetters]);
-    setHangMan([...hangMan]);
   };
 
   return (
@@ -43,8 +23,18 @@ const Form = ({
   );
 };
 
-const Guess = ({ guessedLetters }) => {
-  const displayGuess = guessedLetters.map((el, index) => {
+const Guess = ({ secretWord, guess, guessedLetters, hangMan }) => {
+  if (guess != "" && !secretWord.includes(guess)) {
+    guessedLetters.current.push(hangMan.current.shift());
+  } else {
+    secretWord.forEach((letter, index) => {
+      if (guess == letter) {
+        guessedLetters.current[index] = guess;
+      }
+    });
+  }
+
+  const displayGuess = guessedLetters.current.map((el, index) => {
     return <span key={index}>{el} </span>;
   });
 
@@ -62,26 +52,23 @@ export default function App() {
   */
   const secretWord = ["b", "a", "n", "a", "n", "a"];
   const [guess, setGuess] = useState("");
-  /* initialise guesseLetters state to be an array with only - */
+  /* initialise guesseLetters to be an array with only - */
   const startGuess = ["_", "_", "_", "_", "_", "_"];
-  const [guessedLetters, setGuessedLetters] = useState(startGuess);
-  /* initialise hangMan state to be an array the diff symbols */
+  const guessedLetters = useRef(startGuess);
+  /* initialise hangMan to be an array with hangSymbols*/
   const hangSymbols = ["(", "凸", "ಠ", "益", "ಠ", ")", "凸"];
-  const [hangMan, setHangMan] = useState(hangSymbols);
+  const hangMan = useRef(hangSymbols);
 
   return (
     <div>
       <h1> Welcome to Hangman!</h1>
-      <Form
+      <Form guess={guess} setGuess={setGuess} />
+      <Guess
         secretWord={secretWord}
         guess={guess}
-        setGuess={setGuess}
         guessedLetters={guessedLetters}
-        setGuessedLetters={setGuessedLetters}
         hangMan={hangMan}
-        setHangMan={setHangMan}
       />
-      <Guess guessedLetters={guessedLetters} />
     </div>
   );
 }
